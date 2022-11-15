4d9e22d7928626213eb602d176bf0409ffc318ef
Amappola <anam.porrasp@gmail.com>
2022-11-14 19:09:21 -0500

    Adding first events

--- src/components/ToDoItem.jsx
+++ src/components/ToDoItem.jsx
@@ -2,11 +2,15 @@ import React from 'react';
 import './styles/ToDoItem.css'
 
 function ToDoItem(props) {
+  const onComplete = () => alert(`You have completed ${props.text}`);
+
+  const onDelete = () => alert(`You have deleted ${props.text}`);
+
   return (
     <li>
-      <span><i className="fa-solid fa-check"></i></span>
+      <span onClick={onComplete}><i className="fa-solid fa-check"></i></span>
       <p>{props.text}</p>
-      <span><i className="fa-solid fa-xmark"></i></span>
+      <span onClick={onDelete}><i className="fa-solid fa-xmark"></i></span>
     </li>
   );
 }
--- src/components/ToDoSearch.jsx
+++ src/components/ToDoSearch.jsx
@@ -2,8 +2,16 @@ import React from 'react';
 import './styles/ToDoSearch.css'
 
 function ToDoSearch() {
+  const onSearchValueChange = (event) => {
+    console.log(event.target.value)
+  }
+
   return (
-    <input placeholder='Search your To Do' type='text'/>
+    <input
+      placeholder='Search your To Do'
+      type='text'
+      onChange={onSearchValueChange}
+    />
   );
 }
 
