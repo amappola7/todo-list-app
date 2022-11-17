cfec990d83fe4368427e20472f7cab7ebfa46e51
Amappola <anam.porrasp@gmail.com>
2022-11-17 06:53:18 -0500

    Creating functionality to To Do's searcher

--- src/App.jsx
+++ src/App.jsx
@@ -19,13 +19,24 @@ function App() {
   const [toDoList, setToDoList] = useState(defaultTodos)
 
   const completedToDos = toDoList.filter(toDo => toDo.completed).length;
+  let searchedToDos = [];
+
+  if (searchValue.length === 0) {
+    searchedToDos = toDoList;
+  } else  {
+    const lowerSearchValue = searchValue.toLowerCase();
+    searchedToDos = toDoList.filter((toDo) => {
+      toDo.text = toDo.text.toLowerCase()
+      return toDo.text.includes(lowerSearchValue);
+    })
+  }
 
   return (
     <div>
       <ToDoCounter pendingToDos={toDoList.length} completedToDos={completedToDos}/>
       <ToDoSearch searchValue={searchValue} setSearchValue={setSearchValue} />
       <ToDoList>
-        {toDoList.map(todo => (<ToDoItem key={todo.text} text={todo.text}/>))}
+        {searchedToDos.map(todo => (<ToDoItem key={todo.text} text={todo.text}/>))}
       </ToDoList>
       <CreateToDoButton/>
     </div>
